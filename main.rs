57d43523d24569a34f76912d93775add9fbea194
Bambobello <bellobambo21@gmail.com>
2026-03-20 00:39:23 +0100

    init

---
+++ main.rs
@@ -0,0 +1,28 @@
+fn main(){
+    let x : i32 = -42;
+    let y : u64 = 100;
+
+    println!("Signed Integer : {}", x);
+    println!("Unsigned Integer : {}", y);
+
+
+
+    let e : i32 = 213456789;
+    let i : i64 = 123456789012345678;
+
+    println!("Maximium value of i32 : {}", e);
+    println!("Maximum value of i64 : {}", i);
+
+
+    let pi : f64 = 3.14;
+
+    println!("Value of pi : {}", pi);
+
+    let is_snowing : bool = true;
+    println!("is it snowing? : {}", is_snowing);
+
+
+    let letter : char = 'a';
+    println!("First letter of the alphabet is : {}", letter);
+
+}
