f33a52d28bd1f2b3a397788ede00e03c80cc9cd4
Bambobello <bellobambo21@gmail.com>
2026-03-21 23:54:28 +0100

    shadowing

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, 🦀🦀 from cargo!");
+    let x = 5;
+    
+    const Y : i32 = 10;
+
+    println!("The value of x is: {}", x);
+    println!("The value of Y is: {}",  Y);
+    println!("The value of PI is: {}",  PI);
+    println!("The value of THREE_HOURS_IN_SECONDS is: {}",  THREE_HOURS_IN_SECONDS);
 }
+
+
+const PI : f64 = 3.14159;
+const THREE_HOURS_IN_SECONDS : u32 = 60 * 60 * 3;
--- main.rs
+++ main.rs
@@ -1,61 +1,17 @@
-// fn main (){
-//     let s1 = String::from("Rust");
-//     let len = calculate_length(&s1);
+fn main() {
+    println!("Hello, 🦀🦀 from cargo!");
+    let x = 5;
 
-//     println!("The length of '{}' is {}.", s1, len);
-// }
+    let x = x + 1;
+    let x = x * 1000;
 
-// fn calculate_length(s: &String) -> usize{
-//     s.len()
-// }
-
-// fn main(){
-//     let s1 = String::from("Hello");
-//     let len = calculate_length(&s1);
-//     println!("The length of '{}' is {}.", s1, len);
-
-// }
-
-// fn calculate_length(s: &String) -> usize{
-//     s.len()
-// }
-
-
-
-fn main (){
-    // let mut _x = 5;
-    // let _r = &mut _x;
-    // *_r += 1;
-    // *_r -= 3;
-
-    // println!("The value of _x is: {}", _x);
-    // println!("The value of _r is: {}", _r);
-
-
-    let mut account = BankAccount{
-        owner : "Alice".to_string(),
-        balance : 150.55,
-    };
-
-    account.check_balance();
-    account.withdraw(45.25);
-    account.check_balance();
-
-
-}
-
-struct BankAccount{
-    owner: String,
-    balance: f64,
-}
-
-impl BankAccount {
-    fn withdraw(&mut self, amount : f64){
-        println!("Withdrawing ${} from the account, owned by {}", amount, self.owner);
-        self.balance -= amount;
+    {
+        let x = x * 2;
+        println!("The Value of x in the inner scope : {x}");
     }
 
-    fn check_balance(&self){
-        println!("Account owned by {} has The current balance of ${}", self.owner, self.balance);
-    }
+    println!("Normal Valur of x : {}", x)
 }
+
+const PI: f64 = 3.14159;
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
