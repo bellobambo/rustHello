0639fcae2fc7cd4b6e83b174069c47ed2350db14
Bambobello <bellobambo21@gmail.com>
2026-03-23 22:41:36 +0100

    error handling

--- main.rs
+++ main.rs
@@ -1,185 +1,47 @@
-#![allow(warnings)]
-
-fn main() {
-    // let age : u16 = 18;
-    // if age >= 18{
-    //     println!("You can drive a car");
-    // }else{
-    //     println!("You cannot drive a car");
-    // }
-
-    // let number = 6;
-
-    // if number % 4 == 0{
-    //     println!("The number is: {}", 4);
-
-    // }else if number % 3 == 0{
-    //     println!("The number is: {}", 3);
-    // }else if number % 2 == 0{
-    //     println!("The number is: {}", 2);
-    // }else{
-    //     println!("The number is: {}", number);
-    // }
-
-    // let condition = false;
-    // let number = if condition {5} else {6};
-
-    // println!("The value of number is: {}", number);
-
-    // loop{
-    //     println!("This is an infinite loop");
-    // }
-
-    // let mut counter = 0;
-
-    // let result = loop {
-
-    //     counter += 1;
-
-    //     if counter == 20{
-    //         break counter - 100;
-    //     };
-
-    // };
-
-    // println!("The value of counter is: {}", result);
-
-    // let mut count = 0;
-    // 'counting_up: loop {
-    //     println!("count = {count}");
-    //     let mut remaining = 10;
-
-    //     loop {
-    //         println!("remaining = {remaining}");
-    //         if remaining == 9 {
-    //             break;
-    //         }
-    //         if count == 2 {
-    //             break 'counting_up;
-    //         }
-    //         remaining -= 1;
-    //     }
-
-    //     count += 1;
-    // }
-    // println!("End count = {count}");
-
-    // let mut number = 3;
-    // while number != 0 {
-    //     println!("{number}!");
-    //     number -= 1;
-    //     break;
-
-    // }
-    // println!("LIFTOFF!!!");
-
-    // let a = [1,2,3,4,5,6];
-    // let b = ["a", "b", "c", "d", "e", "f"];
-
-    // for letter in b {
-    //     println!("The value is: {}", letter);
-    // }
-
-    // let rect = (200, 500);
-
-    // struct Book {
-    //     title: String,
-    //     author: String,
-    //     pages: String,
-    //     avaliable: String,
-
-    // }
-
-    // struct User{
-    //     active : bool,
-    //     username : String,
-    //     email : String,
-    //     sign_in_count : u64,
-    // }
-
-    // let mut user1 = User{
-    //     active : true,
-    //     username : String::from("john_doe"),
-    //     email : String::from("bellobambo21@gmail.com"),
-    //     sign_in_count : 1,
-    // };
-
-    // // user1.email = String::from("anotheremail.com");
-    // println!("User 1: {}", user1.email);
-
-    // fn build_user(email : String, username : String) -> User{
-    //     User{
-    //         active : true,
-    //         username : username,
-    //         email : email,
-    //         sign_in_count : 0,
-    //     }
-    // }
-
-    // let user2 = User{
-    //     email : String::from("anotheremail@gmail.com"),
-    //     ..user1
-
-    // };
-
-    // println!("User 2: {}", user2.email);
-
-    // struct Color(i32, i32, i32);
-    // struct Point(i32, i32, i32);
-
-    // let black = Color(0, 0, 0);
-    // let white = Color(255, 255, 255);
-
-    // struct AlwaysEqual;
-    // let subject = AlwaysEqual;
-
-
-
-    let _four = IPAddrKind::V4;
-    let _six = IPAddrKind::V6;
-
-    fn route(ip_kind: IPAddrKind) {
-
+fn divideResult (numerator : f64, denominator : f64)-> Result<f64, String>{
+    if denominator == 0.0 {
+        Err("Error: Division by zero".into())
+    }else{
+        Ok(numerator/denominator)
     }
+}
 
-    route(IPAddrKind::V4);
-    route(IPAddrKind::V6);
-
-
-    enum IPAddrKind {
-        V4(),
-        V6(),
-    }
-
-
-    enum IPAddr {
-        V4(u8, u8,u8,u8),
-        V6(String),
-    }
-
-    let home = IPAddr::V4(127, 0, 0, 1);
-    let loopback = IPAddr::V6(String::from("::1"));
-
-
-
-
-
-    // struct IPAddr{
-    //     kind : IpAddrKind,
-    //     address : String,
-    // }
-
-    // let home = IPAddr{
-    //     kind : IPAddrKind::V4,
-    //     address : String::from("127.0.0.1")
-    // }
-
-    // let loopback = IPAddr{
-    //     kind : IPAddrKind::V6,
-    //     address : String::from("::1")
-    // }
-
-
-
+fn main (){
     
+
+match divideResult(100.23, 00.0){
+    Ok(result) => println!("Result: {}", result),
+    Err(e) => println!("Issues Day: {}", e),
+}
+
+//    enum OPTION<T>{
+//     Some(T),
+//     None,
+//    }
+
+//    enum Result<T,E>{
+//     Ok(T),
+//     Err(E), 
+//    }
+
+//    fn divide (numerator : f64, demoninator : f64)-> OPTION<f64>{
+//     if demoninator == 0.0 {
+//         OPTION::None
+//     }else{
+//         OPTION::Some(numerator/demoninator)
+//     }
+//    }
+
+
+
+    println!("Hello, world!");
+}
+
+
+fn divideOption (numerator : f64, demoninator : f64)-> Option<f64>{
+    if demoninator == 0.0 {
+        None
+    }else{
+        Some(numerator/demoninator)
+    }
 }
